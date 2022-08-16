e6eb6b8c137b4f3784a0ed84b8a420512dde41cb
Chris Wright <chriswrightdev@gmail.com>
2022-08-16 16:38:27 +0100

    venue about data

--- src/modules/Venue/VenueAbout/VenueAbout.jsx
+++ src/modules/Venue/VenueAbout/VenueAbout.jsx
@@ -1,5 +1,14 @@
+import useVenue from "../../../data/queryHooks/useVenue";
+
+import "./VenueAbout.css";
 function VenueAbout() {
-  return <div>VenueAbout</div>;
+  const venue = useVenue();
+  if (venue.status === "success") {
+    const venueData = venue.data[0];
+    return venueData.venue_about;
+  } else {
+    return null;
+  }
 }
 
 export default VenueAbout;
