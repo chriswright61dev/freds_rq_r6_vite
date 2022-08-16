b8cc94e825f43b38bf68f3a2ab7455123d53d409
Chris Wright <chriswrightdev@gmail.com>
2022-08-16 16:25:30 +0100

    added page structure

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,14 @@ import useVenue from "../data/queryHooks/useVenue";
 function Home() {
   const venue = useVenue();
   if (venue.status === "success") {
-    return <div>we have data - home page</div>;
+    return (
+      <div className="container">
+        <div className="info_column">'col1'</div>
+        <div className="info_column">'col2'</div>
+        <div className="info_column">'col3'</div>
+        <div className="info_column">'col4'</div>
+      </div>
+    );
   } else {
     return "no data";
   }
