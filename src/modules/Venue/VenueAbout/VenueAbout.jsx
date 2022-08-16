3e76dfb8b6fbf3900f66e72a2dc619770c13057f
Chris Wright <chriswrightdev@gmail.com>
2022-08-16 16:41:00 +0100

    updated venue about with correct html render

--- src/modules/Venue/VenueAbout/VenueAbout.jsx
+++ src/modules/Venue/VenueAbout/VenueAbout.jsx
@@ -1,11 +1,11 @@
 import useVenue from "../../../data/queryHooks/useVenue";
-
+import HtmlBox from "../../../components/HtmlBox/HtmlBox";
 import "./VenueAbout.css";
 function VenueAbout() {
   const venue = useVenue();
   if (venue.status === "success") {
     const venueData = venue.data[0];
-    return venueData.venue_about;
+    return <HtmlBox content={venueData.venue_about} className="venue_about" />;
   } else {
     return null;
   }
