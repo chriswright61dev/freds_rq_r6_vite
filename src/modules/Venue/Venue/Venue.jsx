4a9fa4eaf6a746ccf54a84bc0107f67e9b696723
Chris Wright <chriswrightdev@gmail.com>
2022-08-16 16:44:42 +0100

    added data to venue

--- src/modules/Venue/Venue/Venue.jsx
+++ src/modules/Venue/Venue/Venue.jsx
@@ -1,5 +1,21 @@
+import useVenue from "../../../data/queryHooks/useVenue";
 function Venue() {
-  return <div>Venue</div>;
+  const venue = useVenue();
+  if (venue.status === "success") {
+    const venueData = venue.data[0];
+    return (
+      <div>
+        <h1>{venueData.venue_name}</h1>
+        <p> {venueData.venue_address_1}</p>
+        <p> {venueData.venue_address_2}</p>
+        <p> {venueData.venue_address_3}</p>
+        <p> {venueData.venue_postcode}</p>
+        <p> {venueData.venue_telephone}</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default Venue;
