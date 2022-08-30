a7e9dc3adbc82910f2aa2a2b4eb197ef19a365e1
Chris Wright <chriswrightdev@gmail.com>
2022-08-30 22:40:36 +0100

    beer updates date time added

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,9 +1,18 @@
 import { siteSettings } from "../config/config";
-
+import { beersLastChanged } from "../utilities/utilities";
+import useGuestAles from "../data/queryHooks/useGuestAles";
 function Settings() {
+  let beerschanged = null;
+  const guestAles = useGuestAles();
+  if (guestAles.status === "success") {
+    const guestAlesData = guestAles.data;
+    beerschanged = beersLastChanged(guestAlesData);
+  } else {
+  }
   return (
     <div>
       <pre>{JSON.stringify(siteSettings, null, 2)}</pre>
+      Guest Beers {beerschanged};
     </div>
   );
 }
